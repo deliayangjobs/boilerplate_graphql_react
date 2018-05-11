c57f30b51802e630fe41ac5e32bd0876949bf3e7
Delia Yang <deliay@surveymonkey.com>
2018-05-10 17:14:32 -0700

    add auth project

---
+++ auth/webpack.config.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        use: [
+          {
+              loader: 'babel-loader',
+              options: {
+                  presets: ['react']
+              }
+          }
+        ],
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'client/index.html'
+    })
+  ]
+};
